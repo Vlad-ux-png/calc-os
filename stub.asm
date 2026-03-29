c06798370419f178ae0b1093672cbbf4b916880b
Vlad-ux-png <ggcc98765432110@gmail.com>
2026-03-29 16:06:49 +0300

    Update GDT entries in stub.asm
    Refactor GDT entries for code and data segments.

--- stub.asm
+++ stub.asm
@@ -44,9 +44,20 @@ align 4
 gdt_start: 
     dq 0
 gdt_code: 
-    dw 0xffff, 0x0000, 0x9a00, 0x00cf
-gdt_data: 
-    dw 0xffff, 0x0000, 0x9200, 0x00cf
+    dw 0xffff  
+    dw 0x0      
+    db 0x0  
+    db 10011010b
+    db 11001111b
+    db 0x0       
+
+gdt_data:
+    dw 0xffff
+    dw 0x0     
+    db 0x0      
+    db 10010010b 
+    db 11001111b 
+    db 0x0    
 gdt_end:
 
 gdt_ptr:
